5bf7c52fd09558017575461ca5e8b90aa90850c3
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-01-16 14:15:10 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+	<ul>
+		<li>Nanda</li>
+		<li>Iqbal</li>
+	</ul>
 
 </body>
 </html>
